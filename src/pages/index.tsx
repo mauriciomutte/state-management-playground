7218af1910044d5fa7f1f5700ce35b46e8b1634d
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-08 14:57:00 -0300

    feat: handle states using useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
 import { NextPageWithLayout } from '@/pages/_app';
-import SignIn from '@/components/SignIn';
 import Layout from '@/components/Layout';
+import SignIn from '@/components/SignIn';
+import SignUp from '@/components/SignUp';
+import OneTimePassword from '@/components/OneTimePassword';
+import ResetPassword from '@/components/ResetPassword';
+
+type StateComponentsType = {
+	login: (onStateChange: any) => JSX.Element;
+	signUp: (onStateChange: any) => JSX.Element;
+	oneTimePassword: (onStateChange: any) => JSX.Element;
+	resetPassword: (onStateChange: any) => JSX.Element;
+};
+
+export type AuthStates = keyof StateComponentsType;
 
 const Home: NextPageWithLayout = () => {
-	return <SignIn />;
+	const [currentState, setCurrentState] = useState<AuthStates>('login');
+
+	const componentByState: StateComponentsType = {
+		login: SignIn,
+		signUp: SignUp,
+		oneTimePassword: OneTimePassword,
+		resetPassword: ResetPassword,
+	};
+
+	const handleChangeState = (state: AuthStates) => setCurrentState(state);
+
+	const ComponentToRender = componentByState[currentState];
+
+	return <ComponentToRender onStateChange={handleChangeState} />;
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
