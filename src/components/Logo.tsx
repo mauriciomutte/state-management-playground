15b5bf9144b4d249a9c1b5b2b40a986b85ee90a7
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-08 10:36:58 -0300

    feat: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+	return (
+		<a
+			href="#"
+			className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+		>
+			<img className="w-8 h-8 mr-2" src="/logo.svg" alt="logo" />
+			Camaleao
+		</a>
+	);
+};
+
+export default Logo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import type { NextPage } from 'next';
 
 import SignIn from '@/components/SignIn';
+import Logo from '@/components/Logo';
 
 const Home: NextPage = () => {
 	return (
 		<section className="bg-gray-50">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<a
-					href="#"
-					className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
-				>
-					<img className="w-8 h-8 mr-2" src="/logo.svg" alt="logo" />
-					Camaleao
-				</a>
+				<Logo />
 				<div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<SignIn />
