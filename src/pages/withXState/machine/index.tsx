b8b8d07fc74d05c539603c07abe3f23975531b6f
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-12 17:42:29 -0300

    feat: create auth machine

---
+++ src/pages/withXState/machine/index.tsx
@@ -0,0 +1,24 @@
+import { createMachine } from 'xstate';
+
+export const authMachine = createMachine({
+	id: 'authMachine',
+	initial: 'login',
+	states: {
+		login: {
+			on: {
+				SIGN_UP: 'signUp',
+				RESET_PASSWORD: 'resetPassword',
+				ONE_TIME_PASSWORD: 'oneTimePassword',
+			},
+		},
+		signUp: {
+			on: { SIGN_IN: 'login' },
+		},
+		resetPassword: {
+			on: { SIGN_IN: 'login' },
+		},
+		oneTimePassword: {
+			on: { SIGN_IN: 'login' },
+		},
+	},
+});
