fb207050a05e2e8f5aa20a54bd15cfe3bfa50255
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-12 20:01:36 -0300

    feat: add title in pages

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,6 +3,7 @@ import { ReactElement, useMemo, useState } from 'react';
 import { NextPageWithLayout } from '@/pages/_app.page';
 import Layout from '@/components/Layout';
 import { AuthStates, getComponentToRender } from '@/utils/getComponentToRender';
+import Head from 'next/head';
 
 const Home: NextPageWithLayout = () => {
 	const [currentState, setCurrentState] = useState<AuthStates>('login');
@@ -14,7 +15,14 @@ const Home: NextPageWithLayout = () => {
 		[currentState]
 	);
 
-	return <ComponentToRender onStateChange={handleChangeState} />;
+	return (
+		<>
+			<Head>
+				<title>with useState</title>
+			</Head>
+			<ComponentToRender onStateChange={handleChangeState} />
+		</>
+	);
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
--- src/pages/withXState/index.page.tsx
+++ src/pages/withXState/index.page.tsx
@@ -5,6 +5,7 @@ import { NextPageWithLayout } from '@/pages/_app.page';
 import Layout from '@/components/Layout';
 import { authMachine } from '@/pages/withXState/machine';
 import { AuthStates, getComponentToRender } from '@/utils/getComponentToRender';
+import Head from 'next/head';
 
 const getMachineEvents = (event: AuthStates) => {
 	const events = {
@@ -33,7 +34,14 @@ const LoginXState: NextPageWithLayout = () => {
 		[currentState]
 	);
 
-	return <ComponentToRender onStateChange={handleChangeState} />;
+	return (
+		<>
+			<Head>
+				<title>with XState</title>
+			</Head>
+			<ComponentToRender onStateChange={handleChangeState} />
+		</>
+	);
 };
 
 LoginXState.getLayout = function getLayout(page: ReactElement) {
