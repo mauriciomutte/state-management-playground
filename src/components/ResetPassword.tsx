4dc29240a93eea4ac6d64725345a174aa8643fc3
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-15 09:38:25 -0300

    feat: add ResetPassword email form

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -12,6 +12,31 @@ const ResetPassword = ({ onStateChange }: ResetPasswordProps) => {
 				Reset Password
 			</h1>
 
+			<form className="space-y-4 md:space-y-6" action="#">
+				<div>
+					<label
+						htmlFor="email"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						Your email
+					</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+						placeholder="name@company.com"
+					/>
+				</div>
+				<button
+					type="submit"
+					className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+					onClick={() => onStateChange('resetPasswordConfirm')}
+				>
+					Reset password
+				</button>
+			</form>
+
 			<Link onClick={() => onStateChange('login')}>Back to login</Link>
 		</>
 	);
