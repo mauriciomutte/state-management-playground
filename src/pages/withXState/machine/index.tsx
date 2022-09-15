9a7937d033175ba79f834e4e12ccd3d47232c940
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-15 09:40:03 -0300

    feat: add machine context and actions

--- src/pages/withXState/machine/index.tsx
+++ src/pages/withXState/machine/index.tsx
@@ -1,31 +1,48 @@
-import { createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
 
-export const authMachine = createMachine({
-	id: 'authMachine',
-	initial: 'login',
-	predictableActionArguments: true,
-	states: {
-		login: {
-			on: {
-				SIGN_UP: 'signUp',
-				RESET_PASSWORD: 'resetPassword',
-				ONE_TIME_PASSWORD: 'oneTimePassword',
+export const authMachine = createMachine(
+	{
+		id: 'authMachine',
+		initial: 'login',
+		predictableActionArguments: true,
+		context: {
+			error: '',
+		},
+		states: {
+			login: {
+				on: {
+					SIGN_UP: 'signUp',
+					RESET_PASSWORD: 'resetPassword',
+					ONE_TIME_PASSWORD: 'oneTimePassword',
+				},
 			},
-		},
-		signUp: {
-			on: { SIGN_IN: 'login' },
-		},
-		resetPassword: {
-			on: {
-				SIGN_IN: 'login',
-				RESET_PASSWORD_CONFIRM: 'resetPasswordConfirm',
+			signUp: {
+				on: { SIGN_IN: 'login' },
 			},
-		},
-		resetPasswordConfirm: {
-			on: { SIGN_IN: 'login', RESET_PASSWORD: 'resetPassword' },
-		},
-		oneTimePassword: {
-			on: { SIGN_IN: 'login' },
+			resetPassword: {
+				on: {
+					SIGN_IN: 'login',
+					RESET_PASSWORD_CONFIRM: 'resetPasswordConfirm',
+				},
+			},
+			resetPasswordConfirm: {
+				entry: 'setErrorMessage',
+				exit: 'clearError',
+				on: { SIGN_IN: 'login', RESET_PASSWORD: 'resetPassword' },
+			},
+			oneTimePassword: {
+				on: { SIGN_IN: 'login' },
+			},
 		},
 	},
-});
+	{
+		actions: {
+			setErrorMessage: assign(() => ({
+				error: 'Some generic test message',
+			})),
+			clearError: assign(() => ({
+				error: '',
+			})),
+		},
+	}
+);
