33fc08df20aaa0a3a5dbd472f9c9eefca5c81123
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-16 10:34:12 -0300

    feat: add setErrorMessage inside component

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,11 +1,14 @@
 import Link from '@/components/Link';
 import type { AuthStates } from '@/utils/getComponentToRender';
+import { useAuthStore } from '@/pages/withZustand/stores/authStore';
 
 type ResetPasswordProps = {
 	onStateChange: (state: AuthStates) => void;
 };
 
 const ResetPassword = ({ onStateChange }: ResetPasswordProps) => {
+	const { setErrorMessage } = useAuthStore();
+
 	return (
 		<>
 			<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
@@ -31,7 +34,10 @@ const ResetPassword = ({ onStateChange }: ResetPasswordProps) => {
 				<button
 					type="submit"
 					className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-					onClick={() => onStateChange('resetPasswordConfirm')}
+					onClick={() => {
+						onStateChange('resetPasswordConfirm');
+						setErrorMessage('Generic error message testing...');
+					}}
 				>
 					Reset password
 				</button>
