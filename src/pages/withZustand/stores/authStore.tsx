a43db475b894ffbdd5453773afce1097e2619a1c
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-16 10:13:27 -0300

    feat: add AuthStore errorMessage

--- src/pages/withZustand/stores/authStore.tsx
+++ src/pages/withZustand/stores/authStore.tsx
@@ -4,10 +4,14 @@ import { AuthStates } from '@/utils/getComponentToRender';
 
 type AuthStoreType = {
 	state: AuthStates;
+	errorMessage?: string;
 	changeState: (currentState: AuthStates) => void;
+	setErrorMessage: (message: string) => void;
 };
 
 export const useAuthStore = create<AuthStoreType>((set) => ({
 	state: 'login',
+	errorMessage: undefined,
 	changeState: (currentState) => set(() => ({ state: currentState })),
+	setErrorMessage: (message) => set(() => ({ errorMessage: message })),
 }));
